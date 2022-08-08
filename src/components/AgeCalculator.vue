96e27b712598a239bdd9b84db23c438fb33ce70d
Anh Le <ale@codio.com>
2022-08-08 20:05:09 +0000

    added horoscope functionality

--- src/components/AgeCalculator.vue
+++ src/components/AgeCalculator.vue
@@ -8,6 +8,8 @@ export default {
       age: "",
       animal: "",
       zodiac: "",
+      sign: "",
+      horoscope: "",
 
       us: {
         select: false,
@@ -60,10 +62,154 @@ export default {
       }
     },
 
+    getHoroscope() {
+      if (this.us.select) {
+        let birthDate = new Date(this.birthday);
+        var year = birthDate.getFullYear();
+        var month = birthDate.getMonth();
+        var date = birthDate.getDate();
+
+        if (month === 0) {
+          if (date >= 19 && date <= 31) {
+            this.sign = "Aquarius";
+          }
+        }
+        if (month === 1) {
+          if (date >= 1 && date <= 17) {
+            this.sign = "Aquarius";
+          }
+        }
+
+        if (month === 1) {
+          if (date >= 18 && date <= 29) {
+            this.sign = "Pisces";
+          }
+        }
+        if (month === 2) {
+          if (date >= 1 && date <= 19) {
+            this.sign = "Pisces";
+          }
+        }
+
+        if (month === 2) {
+          if (date >= 20 && date <= 31) {
+            this.sign = "Aries";
+          }
+        }
+        if (month === 3) {
+          if (date >= 1 && date <= 18) {
+            this.sign = "Aries";
+          }
+        }
+
+        if (month === 3) {
+          if (date >= 19 && date <= 30) {
+            this.sign = "Taurus";
+          }
+        }
+        if (month === 4) {
+          if (date >= 1 && date <= 19) {
+            this.sign = "Taurus";
+          }
+        }
+
+        if (month === 4) {
+          if (date >= 20 && date <= 31) {
+            this.sign = "Gemini";
+          }
+        }
+        if (month === 5) {
+          if (date >= 1 && date <= 19) {
+            this.sign = "Gemini";
+          }
+        }
+
+        if (month === 5) {
+          if (date >= 20 && date <= 30) {
+            this.sign = "Cancer";
+          }
+        }
+        if (month === 6) {
+          if (date >= 1 && date <= 21) {
+            this.sign = "Cancer";
+          }
+        }
+
+        if (month === 6) {
+          if (date >= 22 && date <= 31) {
+            this.sign = "Leo";
+          }
+        }
+        if (month === 7) {
+          if (date >= 1 && date <= 21) {
+            this.sign = "Leo";
+          }
+        }
+
+        if (month === 7) {
+          if (date >= 22 && date <= 31) {
+            this.sign = "Virgo";
+          }
+        }
+        if (month === 8) {
+          if (date >= 1 && date <= 21) {
+            this.sign = "Virgo";
+          }
+        }
+
+        if (month === 8) {
+          if (date >= 22 && date <= 30) {
+            this.sign = "Libra";
+          }
+        }
+        if (month === 9) {
+          if (date >= 1 && date <= 21) {
+            this.sign = "Libra";
+          }
+        }
+
+        if (month === 9) {
+          if (date >= 22 && date <= 31) {
+            this.sign = "Scorpio";
+          }
+        }
+        if (month === 10) {
+          if (date >= 1 && date <= 20) {
+            this.sign = "Scorpio";
+          }
+        }
+
+        if (month === 10) {
+          if (date >= 21 && date <= 30) {
+            this.sign = "Sagittarius";
+          }
+        }
+        if (month === 11) {
+          if (date >= 1 && date <= 20) {
+            this.sign = "Sagittarius";
+          }
+        }
+
+        if (month === 11) {
+          if (date >= 21 && date <= 31) {
+            this.sign = "Capricorn";
+          }
+        }
+        if (month === 0) {
+          if (date >= 1 && date <= 18) {
+            this.sign = "Capricorn";
+          }
+        }
+
+        this.horoscope = "Your astrological sign is " + this.sign + ".";
+      }
+    },
+
     selectUs() {
       this.us.select = !this.us.select;
       this.age = "";
       this.zodiac = "";
+      this.horoscope = "";
     },
   },
 };
@@ -84,10 +230,12 @@ export default {
       <button class="calculate" @click.prevent="getZodiacSign">
         Zodiac Sign
       </button>
+      <button class="calculate" @click.prevent="getHoroscope">Horoscope</button>
     </form>
 
     <h1>{{ age }}</h1>
     <h1>{{ zodiac }}</h1>
+    <h1>{{ horoscope }}</h1>
   </section>
 </template>
 
